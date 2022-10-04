823cc1af70b072e136e32ef9a3f58a2847679953
Collin Rentz <rentzcollin@gmail.com>
2022-10-04 14:44:21 -0600

    almost done

---
+++ Day_2/back-end-2/server/controller.js
@@ -0,0 +1,39 @@
+const { SocketAddress } = require("net");
+const houses = require("./db.json");
+let globalId = 4;
+
+module.exports = {
+    getHouses: (req, res) => {
+        res.status(200).send(houses);
+    },
+    deleteHouse: (req, res) => {
+        const { id } = req.params;
+        const dltHouse = houses.find((obj) => obj.id === Number(id));
+        houses.splice(id -1, 1);
+        console.log(houses);
+        res.send("House deleted.");
+    },
+    createHouse: (req, res) => {
+        let crtHouse = {
+            id: globalId,            
+            address: req.body.address, 
+            price: req.body.price, 
+            imageURL: req.body.imageURL 
+        };
+        houses.push(crtHouse);
+        globalId++;
+        console.log("House created");
+        res.status(200).send(houses);
+    },
+    updateHouse: (req, res) => {
+        const { id } = req.params;
+        const { type } = req.body;
+        const updtIndex = houses.findIndex((obj) => obj.id === Number(id));
+        if (type === "plus") {
+            houses[updtIndex].price += 10000;
+        } else {
+            houses[updtIndex].price -= 10000;  
+        };
+        res.status(200).send(`House price is now ${houses[updtIndex].price}`);
+    }
+}
