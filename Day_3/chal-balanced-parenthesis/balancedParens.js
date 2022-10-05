cb1b8a7453e6e2f93d8971955f5467b0e5d1964b
Collin Rentz <rentzcollin@gmail.com>
2022-10-05 14:02:32 -0600

    part 2

--- Day_3/chal-balanced-parenthesis/balancedParens.js
+++ Day_3/chal-balanced-parenthesis/balancedParens.js
@@ -1,3 +1,8 @@
+//Given a string, return true or false depending on whether that string has balanced parentheses.
+
+//For the purposes of this problem, you only need to worry about parentheses ( and ), not other opening-and-closing marks, like curly brackets, square brackets, or angle brackets.
+
+//For example:
 // Sample Strings
 let sample1 = "This ( is unbalanced."
 let sample2 = "(This (is (a) balanced) string.)"
@@ -5,3 +10,17 @@ let sample3 = "This is () also ) unbalanced."
 let sample4 = "Balanced."
 
 // Write your solution below:
+function balancedParens(str) {
+    let openPar = 0
+    let closedPar = 0
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === "(") {
+        openPar += 1
+      } else if (str[i] === ")") {
+        closedPar += 1
+      }
+    }
+    return openPar === closedPar
+  }
+
+console.log(balancedParens('(boo)'));
