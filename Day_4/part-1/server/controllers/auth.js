d49c23a2a92d5728da47d1a7fd77a40bb5e9c27f
Collin Rentz <rentzcollin@gmail.com>
2022-10-06 13:13:02 -0600

    .

---
+++ Day_4/part-1/server/controllers/auth.js
@@ -0,0 +1,26 @@
+const users = []
+
+module.exports = {
+    login: (req, res) => {
+      console.log('Logging In User')
+      console.log(req.body)
+      const { username, password } = req.body
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username && users[i].password === password) {
+          res.status(200).send(users[i])
+        }
+      }
+      res.status(400).send("User not found.")
+    },
+    register: (req, res) => {
+        console.log('Registering User')
+        const { username, email, firstName, lastName, password } = req.body
+        const bcrypt = require('bcryptjs');
+        const bcrypt = require('bcryptjs');
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync("B4c0/\/", salt);
+        console.log(req.body)
+        users.push(req.body)
+        res.status(200).send(req.body)
+    }
+}
